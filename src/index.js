a71bc6ca702f8504a0287ac033a6c7fa8506570b
enitharmon <hecateenitharmon@gmail.com>
2018-09-25 12:31:54 +0300

    zeros/fix; implement getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function getZerosCount(number) {
-  // your implementation
+let getZerosCount = number => {
+    let zeroCount = 0;
+    let multiplier = 1;
+    for (let i = 0; i < number; i++) {
+        let countNum = number / (multiplier * 5);
+        if (countNum <= 0) {
+            break;
+        }
+        zeroCount = Math.floor(zeroCount + countNum);
+        multiplier = multiplier * 5;
+    }
+    return zeroCount;
 }
+
+module.exports = getZerosCount;
